f4b21bd24d3de9dd4f8d9fcc144636f84d69bf39
Muhammad Abdullah Zahid <119707334+abdullah1904@users.noreply.github.com>
2025-03-23 22:58:03 +0500

    API Key security issue fixed

--- firebase/client.ts
+++ firebase/client.ts
@@ -2,17 +2,15 @@ import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
 const firebaseConfig = {
-    apiKey: "AIzaSyD_F0vCRWopy5PvqOj7BVabCSjURSNli_U",
-    authDomain: "prepwise-6c4d1.firebaseapp.com",
-    projectId: "prepwise-6c4d1",
-    storageBucket: "prepwise-6c4d1.firebasestorage.app",
-    messagingSenderId: "139626185032",
-    appId: "1:139626185032:web:0431954bb608d75ed0f37d",
-    measurementId: "G-0TXKP9FZ7B"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
